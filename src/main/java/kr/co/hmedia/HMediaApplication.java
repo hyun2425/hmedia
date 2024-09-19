ef7db55999b30528e30cff6a5540f65c6e4de636
hyun2425 <hsj@infotech.co.kr>
2024-09-19 16:04:21 +0900

    first commit

---
+++ src/main/java/kr/co/hmedia/HMediaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/hmedia/HMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
